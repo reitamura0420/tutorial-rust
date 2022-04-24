716a4f6f46811826acb63a90bb8b5905bbd62b70
田村黎 <rei0420.run@gmail.com>
2022-04-25 08:33:13 +0900

    12章終わり

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,10 +1,12 @@
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
 pub struct Config {
-  query: String,
-  filename: String,
+  pub query: String,
+  pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,8 +16,35 @@ impl Config {
     }
     let query = args[1].clone();
     let filename = args[2].clone();
-    Ok(Config { query, filename })
+    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+    Ok(Config {
+      query,
+      filename,
+      case_sensitive,
+    })
   }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut list = Vec::new();
+  for line in contents.lines() {
+    if line.contains(query) {
+      list.push(line);
+    };
+  }
+  list
+}
+
+pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut list = Vec::new();
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      list.push(line);
+    };
+  }
+  list
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -24,7 +53,44 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let mut contents = String::new();
   f.read_to_string(&mut contents)?;
 
-  println!("With text:\n{}", contents);
+  let result = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_insensitive(&config.query, &contents)
+  };
+
+  for line in result {
+    println!("{}", line);
+  }
 
   Ok(())
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_insensitive(query, contents)
+    );
+  }
 }
