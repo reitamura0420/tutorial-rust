a0b95c3a95149248dff56e9ba9beab8b345e7872
田村黎 <rei0420.run@gmail.com>
2022-06-30 23:47:57 +0900

    fix: DB構成追加

---
+++ three_architecture/src/schema.rs
@@ -0,0 +1,26 @@
+table! {
+    tasks (id) {
+        id -> Integer,
+        name -> Text,
+        user_id -> Integer,
+        term -> Varchar,
+        update_term_count -> Integer,
+        is_completed -> Bool,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Integer,
+        mail_address -> Varchar,
+        last_name -> Varchar,
+        enable -> Bool,
+    }
+}
+
+joinable!(tasks -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    tasks,
+    users,
+);
